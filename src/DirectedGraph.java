cda54bfb4aee13db92086d8e3f6762e56946d591
Wiggy boy Lindholm <wiggy0boy@gmail.com>
2018-12-12 09:07:17 +0100

    Added documentation to DirectedGraph

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -12,6 +12,7 @@
 import java.lang.reflect.Array;
 import java.util.*;
 
+@SuppressWarnings("unchecked")
 public class DirectedGraph<E extends Edge> {
 
 	
@@ -21,12 +22,30 @@ public class DirectedGraph<E extends Edge> {
 
 	
 	
+	/**
+	 * Creates a directed Graph with <tt>noOfNodes</tt>
+	 * number of nodes. Each node is represented by a
+	 * unique node number (<tt>nodeNo</tt>).
+	 * 
+	 * @param noOfNodes the number of nodes the graph should have.
+	 */
 	public DirectedGraph(int noOfNodes) {
 		this.nodeCount = noOfNodes;
 	}
 	
 	
 	
+	/**
+	 * Adds an edge to the graph connecting two nodes together.
+	 * 
+	 * This method (and thus graph) does not validate 
+	 * the edges to any deep extent. It is thus possible
+	 * to add an edge which has the same source and destination.
+	 * Be aware, such edges could cause the graph to malfunction.
+	 * 
+	 * @param e	the edge to add to the graph.
+	 * @throws	NullPointerException if the edge <tt>e</tt> is null.
+	 */
 	public void addEdge(E e) {
 		if (e == null) throw new NullPointerException();
 		edges.add(e);
@@ -34,8 +53,24 @@ public class DirectedGraph<E extends Edge> {
 	
 	
 	
+	/**
+	 * Finds the shortest path from <tt>sourceNodeNo</tt>
+	 * to the <tt>destNodeNo</tt> using Dijkstra's algorithm.
+	 * 
+	 * Be aware, the Dijkstra's algorithm is a greedy algorithm
+	 * and does not always find the best/shortest path.
+	 * 
+	 * 
+	 * @param sourceNodeNo	the node number for the source node.
+	 * @param destNodeNo	the node number for the destination node.
+	 * @return				returns an iterator over the shortest path
+	 * 						from the source node to the destination node.
+	 * 						If the source and destination node is the same,
+	 * 						the iterator will be empty. If a path does not
+	 * 						exist this will return null.
+	 */
 	public Iterator<E> shortestPath(int sourceNodeNo, int destNodeNo) {
-		LinkedList<E>[] edgesByNode = getEdgesByOrigin();
+		List<E>[] edgesByNode = getEdgesByOrigin();
 		DijkstraNode[] ssfNodes = (DijkstraNode[]) Array.newInstance(DijkstraNode.class, this.nodeCount);
 		
 		PriorityQueue<DijkstraNode> nodeQueue = new PriorityQueue<>();
@@ -53,6 +88,14 @@ public class DirectedGraph<E extends Edge> {
 		while (!nodeQueue.isEmpty()) {
 			DijkstraNode node = nodeQueue.poll();
 			
+			/*
+			 * The node we extract will lock it's shortest/cheapest/lightest path.
+			 * Thus if the node we've extracted is the destination node.
+			 * We've found the shortest path (accomplishable by the algorithm).
+			 * 
+			 * Then we can "backtrack" to the source node, and collect/sum the
+			 * path (edges) and return it as an iterator.
+			 */
 			if (node.getNodeNo() == destNodeNo) {
 				LinkedList<E> path = new LinkedList<>();
 				
@@ -64,10 +107,21 @@ public class DirectedGraph<E extends Edge> {
 				return path.iterator();
 			}
 			
+			
+			/*
+			 * Otherwise...
+			 * For all edges on the extracted node.
+			 * Check if the path to the connected nodes are
+			 * shorter/cheaper/lighter than the already known
+			 * shortest/cheapest/lightest path to that node.
+			 * 
+			 * If so, save the new and better path.
+			 */
 			for (E e : edgesByNode[node.getNodeNo()]) {
 				DijkstraNode nextNode = ssfNodes[e.getDest()];
 				double newNextNodeWeight = node.getWeightToNode() + e.getWeight();
 				
+				// Null value means no connection yet, equivalent to infinite weight.
 				if (nextNode == null || newNextNodeWeight < nextNode.getWeightToNode()) {
 					DijkstraNode newNode = new DijkstraNode(e, newNextNodeWeight);
 					
@@ -77,10 +131,23 @@ public class DirectedGraph<E extends Edge> {
 			}
 		}
 		
+		/*
+		 * If no path was found, no path exists.
+		 * Thus return null.
+		 */
 		return null;
 	}
 	
-	private LinkedList<E>[] getEdgesByOrigin() {
+	
+	
+	/**
+	 * Returns an array of all edges grouped by connected node.
+	 * Uses the node numbers (<tt>nodeNo</tt>) as indices for
+	 * the connected edges to that node.
+	 * 
+	 * @return returns an array of Lists containing all edges.
+	 */
+	private List<E>[] getEdgesByOrigin() {
 		LinkedList<E>[] edgesByNode = new LinkedList[this.nodeCount];
 		
 		for (int i = 0; i < edgesByNode.length; i++) {
@@ -133,8 +200,9 @@ public class DirectedGraph<E extends Edge> {
 		}
 		
 	}
-
-
+	
+	
+	
 	public class DijkstraNode implements Comparable<DijkstraNode> {
 		
 		
