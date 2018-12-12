78b73a2565c7c824b327e44c26b14ca15dd0376a
Wiggy boy Lindholm <wiggy0boy@gmail.com>
2018-12-12 08:47:42 +0100

    Implemented Dijkstra's algorithm

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -9,6 +9,7 @@
  *
  */
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
@@ -33,8 +34,64 @@ public class DirectedGraph<E extends Edge> {
 	
 	
 	
-	public Iterator<E> shortestPath(int from, int to) {
+	public Iterator<E> shortestPath(int sourceNodeNo, int destNodeNo) {
+		LinkedList<E>[] edgesByNode = getEdgesByOrigin();
+		DijkstraNode[] ssfNodes = (DijkstraNode[]) Array.newInstance(DijkstraNode.class, this.nodeCount);
+		
+		PriorityQueue<DijkstraNode> nodeQueue = new PriorityQueue<>();
+		
+		/*
+		 * Creates an initial "dummy" Dijkstra's node for the source node.
+		 * Each node Dijkstra's node contains the edge which brought them
+		 * to this node (nodeNo). However the initial one, does not have
+		 * this edge, as it is the starting point. Thus a "dummy" is needed.
+		 */
+		DijkstraNode dummshiet = new DijkstraDummyNode(sourceNodeNo);
+		ssfNodes[sourceNodeNo] = dummshiet;
+		nodeQueue.add(dummshiet);
+		
+		while (!nodeQueue.isEmpty()) {
+			DijkstraNode node = nodeQueue.poll();
+			
+			if (node.getNodeNo() == destNodeNo) {
+				LinkedList<E> path = new LinkedList<>();
+				
+				while (node.getNodeNo() != sourceNodeNo) {
+					path.push(node.getEdge());
+					node = ssfNodes[node.getPreviousNodeNo()];
+				}
+				
+				return path.iterator();
+			}
+			
+			for (E e : edgesByNode[node.getNodeNo()]) {
+				DijkstraNode nextNode = ssfNodes[e.getDest()];
+				double newNextNodeWeight = node.getWeightToNode() + e.getWeight();
+				
+				if (nextNode == null || newNextNodeWeight < nextNode.getWeightToNode()) {
+					DijkstraNode newNode = new DijkstraNode(e, newNextNodeWeight);
+					
+					ssfNodes[newNode.getNodeNo()] = newNode;
+					nodeQueue.add(newNode);
+				}
+			}
+		}
+		
 		return null;
+	}
+	
+	private LinkedList<E>[] getEdgesByOrigin() {
+		LinkedList<E>[] edgesByNode = new LinkedList[this.nodeCount];
+		
+		for (int i = 0; i < edgesByNode.length; i++) {
+			edgesByNode[i] = new LinkedList<>();
+		}
+		
+		for (E e : edges) {
+			edgesByNode[e.from].add(e);
+		}
+		
+		return edgesByNode;
 	}
 	
 	
@@ -60,5 +117,70 @@ public class DirectedGraph<E extends Edge> {
 	}
 	
 	
+	
+	private class DijkstraDummyNode extends DijkstraNode {
+		
+		int nodeNo;
+		
+		public DijkstraDummyNode(int nodeNo) {
+			super(null, 0);
+			this.nodeNo = nodeNo;
+		}
+		
+		@Override
+		public int getNodeNo() {
+			return this.nodeNo;
+		}
+		
+	}
 
+
+	public class DijkstraNode implements Comparable<DijkstraNode> {
+		
+		
+		
+		E edge;
+		double weightToNode;
+		
+		
+		
+		public DijkstraNode(E edge, double weightToNode) {
+			this.edge = edge;
+			this.weightToNode = weightToNode;
+		}
+		
+		
+		
+		public E getEdge() {
+			return edge;
+		}
+		
+		
+		
+		public double getWeightToNode() {
+			return weightToNode;
+		}
+		
+		
+		
+		public int getPreviousNodeNo() {
+			return edge.from;
+		}
+		
+		
+		
+		public int getNodeNo() {
+			return edge.to;
+		}
+		
+		
+		
+		@Override
+		public int compareTo(DijkstraNode node) {
+			return (int) Math.signum(this.getWeightToNode() - node.getWeightToNode());
+		}
+		
+		
+		
+	}
 }
